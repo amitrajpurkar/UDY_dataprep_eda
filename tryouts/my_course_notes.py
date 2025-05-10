7879882745499072ad38bfc93c600e3c2538e6e7
Amit Rajpurkar <amit.rajpurkar@gmail.com>
2025-05-10 17:39:52 -0400

    adding a test python script to get myself started on this course

---
+++ tryouts/my_course_notes.py
@@ -0,0 +1,15 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.align = "l"
+table.field_names = ["Section", "Topic(s)", "take aways"]
+table.add_row(["1. Getting Started", "few","course intro and materials"])
+table.add_row(["2. Intro to Data Science", "few","what parts encompasses data science; expand notes"])
+table.add_row(["3. Scoping a Project", "few","SDLC style outlook on a data science project"])
+table.add_row(["4. Jupyter Notebook", "few","installation, know-how of jupyter notebook; i choose to use PyCharm"])
+table.add_row(["5. Gathering data", "44-47","basics on data sources"])
+print(table)
+
+
+
+

d1cfa33100444c32ab7bc3b4ba925f80614a955c
Amit Rajpurkar <amit.rajpurkar@gmail.com>
2025-05-24 15:15:05 -0400

    adding working examples for reading a csv or excel, adding mkdocs

--- tryouts/my_course_notes.py
+++ tryouts/my_course_notes.py
@@ -2,7 +2,7 @@ from prettytable import PrettyTable
 
 table = PrettyTable()
 table.align = "l"
-table.field_names = ["Section", "Topic(s)", "take aways"]
+table.field_names = ["Section", "Topic(s)", "take-away's"]
 table.add_row(["1. Getting Started", "few","course intro and materials"])
 table.add_row(["2. Intro to Data Science", "few","what parts encompasses data science; expand notes"])
 table.add_row(["3. Scoping a Project", "few","SDLC style outlook on a data science project"])
